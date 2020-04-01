aa1b53f9d456b5f78b97ca3bb24c430d06c22021
Sandra RO <romeroortiz.sandra@gmail.com>
2020-04-01 14:09:39 +0200

    first structure with html and js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,46 @@
 'use strict';
-
+// debugger
 console.log('>> Ready :)');
+
+const cards = document.querySelector('.js-cards');
+const button = document.querySelector('.js-button');
+const numberOfCards = document.querySelector('.js-numberOfCards');
+
+let cardsList = [];
+
+function getServerData(){
+// debugger
+  fetch('https://beta.adalab.es/ejercicios-extra/api/pokemon-cards/4.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // cards.innerHTML = data;
+      cardsList = data;
+      // console.log(cardsList);
+      
+    })
+    .catch(function(err) {
+      console.log("Error al traer los datos del servidor", err);
+    });
+
+
+  paintCards();
+  listenToButtton();
+}
+
+function paintCards() {
+  cards.innerHTML = `<li>nombre: ${cardsList[0].name}</li><li>nombre: ${cardsList[1].name}</li><li>nombre: ${cardsList[2].name}</li><li>nombre: ${cardsList[3].name}</li>
+  `
+  // console.log(cardsList);
+  // cards.innerHTML = cardsList;
+
+}
+
+function listenToButtton(params) {
+  
+  button.addEventListener("click",getServerData);
+}
+
+
+listenToButtton();
