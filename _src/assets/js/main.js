a693c806b4ac7028e4fc0584313f788536cdfc65
Sandra RO <romeroortiz.sandra@gmail.com>
2020-04-09 12:17:57 +0200

    Add selected option functionality

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,21 +4,22 @@ console.log('>> Ready :)');
 
 const cards = document.querySelector('.js-cards');
 const button = document.querySelector('.js-button');
-const numberOfCards = document.querySelector('.js-numberOfCards');
+const numberOfCards = document.querySelectorAll('.js-numberOfCards');
 
 let cardsList = [];
+let numberForUrl = "";
+
+
+// Get data from server
+
 
 function getServerData(){
-// debugger
-  fetch('https://beta.adalab.es/ejercicios-extra/api/pokemon-cards/4.json')
+  fetch(`https://beta.adalab.es/ejercicios-extra/api/pokemon-cards/${numberForUrl}.json`)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      // cards.innerHTML = data;
-      cardsList = data;
-      // console.log(cardsList);
-      
+      cardsList = data;      
     })
     .catch(function(err) {
       console.log("Error al traer los datos del servidor", err);
@@ -29,29 +30,46 @@ function getServerData(){
   listenToButtton();
 }
 
+// Painting 
+
 function paintCards() {
   let cardsPainting = "";
-  // cardsPainting = 'a ver...'
-  // cardsPainting = `<li>nombre: ${cardsList[0].name}</li><li>nombre: ${cardsList[1].name}</li><li>nombre: ${cardsList[2].name}</li><li>nombre: ${cardsList[3].name}</li>`
-  // cardsPainting += for (let i=0; i<cardsList.length; i++){
-  //   `<li>name: ${cardsList[i].name}</li>`;
-  // }
   
   for (let i = 0; i < cardsList.length; i++) {
-    cardsPainting += `<li class="card open">name: ${cardsList[i].name}</li>`;
+    cardsPainting += `<li class="card">
+    <img src="${cardsList[i].image}" alt="${cardsList[i].name}" class="poke-img">
+    <p class="poke-name">${cardsList[i].name}</p> 
+    </li>`;
     
   }
 
-  // console.log(cardsList);
-  // cards.innerHTML = cardsList;
   cards.innerHTML = cardsPainting; 
 
 }
 
-function listenToButtton(params) {
-  
-  button.addEventListener("click",getServerData);
+
+//Functions related to listening
+
+function handleButton() {
+  getServerData();
+  DetectCheckedOption();
 }
 
 
+function DetectCheckedOption(){
+
+  for(let i=0; i< numberOfCards.length; i++){
+    if (numberOfCards[i].checked === true){
+      numberForUrl = numberOfCards[i].value;
+      console.log('valor: ', numberOfCards[i].value, 'constante: ', numberForUrl);
+    }    
+  }
+}  
+
+function listenToButtton() {
+  
+  button.addEventListener("click",handleButton);
+}
+
 listenToButtton();
+DetectCheckedOption();
